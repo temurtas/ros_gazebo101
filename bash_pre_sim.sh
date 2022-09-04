cf5ee6ae9b3d4d4ec445f06db0a068b8e4737567
temurtas <halil.temurtas@metu.edu.tr>
2022-09-05 02:07:20 +0300

    bash scripts added

---
+++ bash_pre_sim.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# A sample Bash script, by Ryan
+
+cd ~/INS-GPS-ROS2
+rosdep install -i --from-path src --rosdistro foxy -y
+export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+
+export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:~/ws/src/gazebo_ros_pkgs/build/gazebo_plugins
+export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:~/gazebo_wheel_slip_plugin/build
+
+colcon build --allow-overriding ht_nav_variables ht_strap_package
+
+. install/setup.bash
